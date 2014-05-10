fd19610e72df600dc0b96a0f2f568c1fdd5b2394
Lechus <lechus@master.pl>
2014-05-10 23:46:10 +0200

    fix code format

--- test/fruitTest.php
+++ test/fruitTest.php
@@ -1,6 +1,9 @@
-<?php use LPP\Shopping\Fruit;
+<?php
 
-class FruitTest extends PHPUnit_Framework_TestCase {
+use LPP\Shopping\Fruit;
+
+class FruitTest extends PHPUnit_Framework_TestCase
+{
 
     /**
      * @var Fruit
@@ -11,7 +14,8 @@ class FruitTest extends PHPUnit_Framework_TestCase {
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp() {
+    protected function setUp()
+    {
         $productName = 'Lemon';
         $priceAndDiscounts = array('0' => 0.50, '11' => 0.45);
         $this->object = new Fruit($productName, $priceAndDiscounts);
@@ -21,36 +25,40 @@ class FruitTest extends PHPUnit_Framework_TestCase {
      * Tears down the fixture, for example, closes a network connection.
      * This method is called after a test is executed.
      */
-    protected function tearDown() {
+    protected function tearDown()
+    {
         
     }
 
     /**
      * @covers LPP\Shopping\Fruit::getProductName
      */
-    public function testGetProductName() {
-        echo PHP_EOL, 'Executing ',  __FUNCTION__, PHP_EOL;
-        
+    public function testGetProductName()
+    {
+        echo PHP_EOL, 'Executing ', __FUNCTION__, PHP_EOL;
+
         $this->assertEquals('Lemon', $this->object->getProductName());
     }
 
     /**
      * @covers LPP\Shopping\Fruit::getPriceAndDiscounts
      */
-    public function testGetPriceAndDiscounts() {
-        echo PHP_EOL, 'Executing ',  __FUNCTION__, PHP_EOL;
-        
+    public function testGetPriceAndDiscounts()
+    {
+        echo PHP_EOL, 'Executing ', __FUNCTION__, PHP_EOL;
+
         $priceAndDiscounts = array('0' => 0.50, '11' => 0.45);
-        
+
         $this->assertEquals($priceAndDiscounts, $this->object->getPriceAndDiscounts());
     }
-    
+
     /**
      * @covers LPP\Shopping\Fruit::getType
      */
-    public function testGetProductType() {
-        echo PHP_EOL, 'Executing ',  __FUNCTION__, PHP_EOL;
-        
+    public function testGetProductType()
+    {
+        echo PHP_EOL, 'Executing ', __FUNCTION__, PHP_EOL;
+
         $this->assertEquals('fruit', $this->object->getProductType());
     }
 
